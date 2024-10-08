76993c452ad48614b3b1c0c603a2485ba66dff1b
piyumal27100 <piyumal27100@gmail.com>
2024-10-09 03:15:11 +0530

    sdf

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -37,13 +37,14 @@ export default function Contact() {
             <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
               <iframe
                 class="position-relative rounded w-100 h-100"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2382.972722253143!2d-6.260296184253791!3d53.3498057799789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e9c6c3cb2d9%3A0x7889cf5a2435bf0!2sDublin%2C%20Ireland!5e0!3m2!1sen!2sus!4v1603794290143!5m2!1sen!2sus"
                 frameborder="0"
                 style={{ minHeight: "350px", border: "0" }}
                 allowfullscreen=""
                 aria-hidden="false"
                 tabindex="0"
               ></iframe>
+
             </div>
             <div class="col-md-6">
               <div class="wow fadeInUp" data-wow-delay="0.2s">
